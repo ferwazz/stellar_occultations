4b5986f5e7db32764ba5a585c466e527533d6c5d
Fernando <fernando.alvarez@uabc.edu.mx>
2021-03-29 22:06:53 -0700

    Add setup

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="stellar_occultations",
+    version="0.1.0",
+    packages=["stellar_occultations"],
+    author="Joel H. Castro-Chacón",
+    python_requires='>=3.6'
+)
