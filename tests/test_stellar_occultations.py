4993d54ce8527d569762bc6acf5fcad2c27ec616
Fernando <fernando.alvarez@uabc.edu.mx>
2021-06-07 19:57:20 -0700

    ✅ Add some new test

--- tests/test_stellar_occultations.py
+++ tests/test_stellar_occultations.py
@@ -1,6 +1,16 @@
 import unittest
 import numpy as np
-from stellar_occultations import cart2pol, calculate_plane, pupilCO, pupil_doble, calculate_separation, calculate_binary_parameters
+from stellar_occultations import (
+    cart2pol,
+    calculate_plane,
+    pupilCO,
+    pupil_doble,
+    calculate_separation,
+    calculate_binary_parameters,
+    calculate_star_radius,
+    pol2cart,
+    calculate_image_grid,
+)
 
 
 class Test_StellarOccultations(unittest.TestCase):
@@ -15,11 +25,23 @@ class Test_StellarOccultations(unittest.TestCase):
         self.object_distance_ua = 50
         self.n_pixels = 4096
         self.plane = 149799.86648859203
+        self.mV = 15
+        self.nEst = 29
+        self.spectral_type = "M2"
 
     def test_cart2pol(self):
         expected_polar_coordinates = (0, 1)
-        output = cart2pol(self.cartesian_x_coordinate, self.cartesian_y_coordinate)
-        assert output == expected_polar_coordinates
+        obtained_polar_coordinates = cart2pol(
+            self.cartesian_x_coordinate, self.cartesian_y_coordinate
+        )
+        assert obtained_polar_coordinates == expected_polar_coordinates
+
+    def test_pol2cart(self):
+        expected_cartesian_coordinates = (-1, 0)
+        obtained_cartesian_coordinates = pol2cart(1, np.pi)
+        np.testing.assert_almost_equal(
+            obtained_cartesian_coordinates, expected_cartesian_coordinates
+        )
 
     def test_calculate_plane(self):
         total_plane_size_obtained = calculate_plane(
@@ -64,13 +86,39 @@ class Test_StellarOccultations(unittest.TestCase):
             ]
         )
         np.testing.assert_array_equal(binary_object_obtained, binary_object_expected)
-    
+
     def test_calculate_separation(self):
         obtained_separation = calculate_separation(self.n_pixels, self.plane, self.object_diameter)
-        expected_separations = 68.35787133882275
+        expected_separations = 68
         assert obtained_separation == expected_separations
 
     def test_calculate_binary_parameters(self):
         obtained_binary_parameters = calculate_binary_parameters(self.object_diameter)
         expected_parameters = (3172.3519672766997, 0, 1625.0, 1899.835519196333)
         assert obtained_binary_parameters == expected_parameters
+
+    def test_calc_rstar(self):
+        obtainded_star_parameters = calculate_star_radius(
+            self.mV, self.spectral_type, self.object_distance_ua
+        )
+        expected_star_parameters = 852.1977173475242
+        assert obtainded_star_parameters == expected_star_parameters
+
+    def test_calculate_image_grid(self):
+        obtained_rho_grid, obtained_phi_grid = calculate_image_grid(3, self.plane)
+        expected_rho_grid = np.array(
+            [
+                [-2.35619449, -1.57079633, -0.78539816],
+                [3.14159265, 0.0, 0.0],
+                [2.35619449, 1.57079633, 0.78539816],
+            ]
+        )
+        expected_phi_grid = np.array(
+            [
+                [105924.50141492, 74899.9332443, 105924.50141492],
+                [74899.9332443, 0.0, 74899.9332443],
+                [105924.50141492, 74899.9332443, 105924.50141492],
+            ]
+        )
+        np.testing.assert_array_almost_equal(obtained_rho_grid, expected_rho_grid)
+        np.testing.assert_array_almost_equal(obtained_phi_grid, expected_phi_grid)
